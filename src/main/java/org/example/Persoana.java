b4642428339ef51eedf9f8fd2f6dbebf69ecfff3
maria.uliniuc <maria.uliniuc@student.upt.ro>
2023-03-23 17:07:28 +0200

    Laboratorul 5

--- src/main/java/org/example/Persoana.java
+++ src/main/java/org/example/Persoana.java
@@ -1,2 +1,23 @@
-package org.example;public class Persoana {
+package org.example;
+
+public class Persoana {
+    private String nume;
+    private int varsta;
+    public String getNume() {
+        return nume;
+    }
+    public int getVarsta() {
+        return varsta;
+    }
+    public Persoana() {}
+    public Persoana(String nume, int varsta) {
+        super();
+        this.nume = nume;
+        this.varsta = varsta;
+    }
+    @Override
+    public String toString() {
+        return nume + ", " + varsta;
+    }
+
 }
